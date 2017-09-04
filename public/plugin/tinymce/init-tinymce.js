f2b74889bd20eeca88aef4a38bb0d1e84df4cf86
SomeRandomCoder <tyron.burkhardt.za@gmail.com>
2017-09-04 21:02:08 +0200

    changed text editor menubar

--- public/plugin/tinymce/init-tinymce.js
+++ public/plugin/tinymce/init-tinymce.js
@@ -1,28 +1,35 @@
 tinymce.init({
 	/* replace textarea having class .tinymce with tinymce editor */
 	selector: "textarea.tinymce",
-	
+
 	/* theme of the editor */
 	theme: "modern",
 	skin: "lightgray",
-	
+
 	/* width and height of the editor */
 	width: "100%",
 	height: 300,
-	
+
 	/* display statusbar */
-	statubar: true,
-	
+	statubar: false,
+	menubar: false,
+
 	/* plugin */
+	// plugins: [
+	// 	"advlist autolink link image lists charmap print preview hr anchor pagebreak",
+	// 	"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+	// 	"save table contextmenu directionality emoticons template paste textcolor"
+	// ],
+
 	plugins: [
-		"advlist autolink link image lists charmap print preview hr anchor pagebreak",
-		"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-		"save table contextmenu directionality emoticons template paste textcolor"
+		" link lists preview hr  pagebreak",
+		"searchreplace visualchars code fullscreen  ",
+		"directionality paste textcolor"
 	],
 
 	/* toolbar */
-	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
-	
+	toolbar: " undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor",
+
 	/* style */
 	style_formats: [
 		{title: "Headers", items: [
